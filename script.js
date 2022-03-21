cf2c1723175510808d9347c47f52521a959b97ed
Orincy Whyte <92553207+oswhyteknits@users.noreply.github.com>
2022-03-21 15:19:22 -0400

    numbers are no longer strings

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let num1 = document.querySelector("#num1");
 let num2 = document.querySelector("#num2");
+
 let answer = document.querySelector("#answer");
 let btnAnswer = document.querySelector(".btn-answer");
 let btnClear = document.querySelector(".btn-clear");
 
-num1.value = "";
-num2.value = "";
-// answer.textContent = "";
 
 let btnClearFunc = () => {
   num1.value = "";
@@ -17,11 +15,17 @@ let btnClearFunc = () => {
 
 btnClear.addEventListener("click", btnClearFunc);
 
+// Numbers were being passed at strings, I used parseInt to the value
 let biggerNum = () => {
+  num1.value = parseInt(num1.value);
+  num2.value = parseInt(num2.value);
+
   if (num1.value > num2.value) {
     num1.style.backgroundColor = "#af77d5";
   } else if (num2.value > num1.value) {
     num2.style.backgroundColor = "#af77d5";
+  } else {
+    return "";
   }
 };
 btnAnswer.addEventListener("click", biggerNum);
