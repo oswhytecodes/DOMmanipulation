4a2ffd99ac9180678e4daf9f5f52c6be73ab642f
Orincy Whyte <92553207+oswhyteknits@users.noreply.github.com>
2022-03-21 22:13:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-let num1 = document.querySelector("#num1");
-let num2 = document.querySelector("#num2");
+let box1 = document.querySelector("#box1");
+let box2 = document.querySelector("#box2");
 
 let answer = document.querySelector("#answer");
 let btnAnswer = document.querySelector(".btn-answer");
 let btnClear = document.querySelector(".btn-clear");
 
-
-let btnClearFunc = () => {
-  num1.value = "";
-  num2.value = "";
-  num1.style.backgroundColor = "";
-  num2.style.backgroundColor = "";
+const btnClearFunc = () => {
+  box1.value = "";
+  box2.value = "";
+  box1.style.backgroundColor = "";
+  box2.style.backgroundColor = "";
 };
 
 btnClear.addEventListener("click", btnClearFunc);
 
-// Numbers were being passed at strings, I used parseInt to the value
-let biggerNum = () => {
-  num1.value = parseInt(num1.value);
-  num2.value = parseInt(num2.value);
+const biggerNum = () => {
+  let numeroUno = Number(box1.value);
+  let numeroDos = Number(box2.value);
 
-  if (num1.value > num2.value) {
-    num1.style.backgroundColor = "#af77d5";
-  } else if (num2.value > num1.value) {
-    num2.style.backgroundColor = "#af77d5";
+  if (numeroUno > numeroDos) {
+    box1.style.backgroundColor = "#af77d5";
+  } else if (numeroUno > numeroDos) {
+    box2.style.backgroundColor = "#af77d5";
   } else {
     return "";
   }
 };
 btnAnswer.addEventListener("click", biggerNum);
+
